ecdd73deac67bf0a065699e95fdcf57b1c55df72
Mr.mshao <mshao_love@126.com>
2017-08-12 17:19:18 +0800

    加载更多

---
+++ src/views/loadmore.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <load-more></load-more>
+  </div>
+</template>
+
+<script>
+import LoadMore from '../components/pull-drop-refresh'
+export default {
+  components: { LoadMore },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  
+</style>
