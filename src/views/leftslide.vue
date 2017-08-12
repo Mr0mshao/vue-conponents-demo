7185ff70626030a2132ec4ff39a598e78eeed915
Mr.mshao <mshao_love@126.com>
2017-08-12 17:19:02 +0800

    左滑动组建

---
+++ src/views/leftslide.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <left-slide></left-slide>
+  </div>
+</template>
+
+<script>
+import LeftSlide from '../components/left-slide'
+export default {
+  components: { LeftSlide },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  
+</style>
